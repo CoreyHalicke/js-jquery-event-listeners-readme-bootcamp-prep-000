b8772b14e9f6a206e35ee8895bda171ee810f74a
Corey H <c.l.halicke@gmail.com>
2018-07-03 00:23:09 +0000

    automatically backed up by learn

--- js/events.js
+++ js/events.js
@@ -22,8 +22,8 @@ function pressIt() {
 
 function submitIt(){
   $('form').on('submit', function() {
-    alert('your form is')
-  }
+    alert('Your form is going to be submitted now.')
+  })
 }
 
 $(document).ready(function(){
@@ -32,4 +32,5 @@ $(document).ready(function(){
   getIt()
   frameIt()
   pressIt()
+  submitIt()
 });
